1ad8d64d13feb96e99229ef15b9899295f19fd2d
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2026-02-07 12:21:20 -0300

    feat: Adicionar ordenação pela data + Melhorar estilo do Filtro + Mensagem resumo da página

--- frontend/src/app/(dashboard)/transacoes/page.tsx
+++ frontend/src/app/(dashboard)/transacoes/page.tsx
@@ -3,13 +3,15 @@
 import { useState } from "react";
 import { transacao, Transaction } from "@/app/core/data/transacao";
 import ModalTransacao from "@/app/components/transacoes/ModalTransacao";
+import { IoIosAlert , IoMdSwap } from "react-icons/io";
+
 
 export default function Transacoes() {
 
 const [filterType, setFilterType] = useState("Todos");
 const [filterStatus, setFilterStatus] = useState("Todos");
-const [filterPeriod, setFilterPeriod] = useState("Últimos 7 dias");
-
+const [filterPeriod, setFilterPeriod] = useState("Todos");
+const [sortAmount, setSortAmount] = useState("Todos");
 const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
 const [openModalTransacao, setopenModalTransacao] = useState(false);
 
@@ -17,7 +19,36 @@ const filteredTransactions = transacao.filter(tx => {
     const typeMatch = filterType === "Todos" || tx.type === filterType;
     const statusMatch = filterStatus === "Todos" || tx.status === filterStatus;
 
-    return typeMatch && statusMatch;
+    let periodMatch = true;
+    if (filterPeriod !== "Todos"){
+        const today = new Date();
+        const dateParts = tx.date.split("/");
+        const date = new Date(
+            parseInt(dateParts[2]),
+            parseInt(dateParts[1]) - 1,
+            parseInt(dateParts[0])
+        );
+
+    if(filterPeriod === "Últimos 7 dias"){
+        const sevenDaysAgo = new Date();
+        sevenDaysAgo.setDate(today.getDate() - 7);
+        periodMatch = date >= sevenDaysAgo && date <= today;
+    }
+
+    if(filterPeriod === "Últimos 30 dias"){
+        const thirtyDaysAgo = new Date();
+        thirtyDaysAgo.setDate(today.getDate() - 30);
+        periodMatch = date >= thirtyDaysAgo && date <= today;
+    }
+}
+
+    return typeMatch && statusMatch && periodMatch;
+});
+
+const sortedTransactions = [...filteredTransactions].sort((a,b) => {
+    if (sortAmount === "Maior para menor") return b.amount - a.amount;
+    if (sortAmount === "Menor para maior") return a.amount - b.amount;
+    return 0;
 });
 
 function handleOpenModal(tx: Transaction){
@@ -38,41 +69,76 @@ function handleCloseModal(){
           Transações
         </h1>
 
-        <div className="flex flex-wrap gap-4 mb-4">
+        <div className="flex items-center gap-3 w-full max-w-xl bg-white border-2 border-primary-1 rounded-xl p-2 shadow-sm">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full ">
+                <IoIosAlert className="text-primary-1 text-[24px]" />
+            </div>
+
+            <p className="text-primary-4 text-sm md:text-base leading-snug">
+                Aqui você acompanha suas transações e investimentos em ativos reais (RWA), com status e detalhes de cada operação.
+            </p>
+        </div>
+
+        <div className="flex flex-col gap-4 mb-4 rounded-[20px]  bg-white min-h-[100px] min-w-[300px] p-6 border-2 border-primary-1 shadow-sm">
+
+            <h1 className="flex items-center justify-center text-[20px] md:text-[24px] font-bold text-primary-4 ">Filtrar por:</h1>
+
+            <div className="flex flex-row items-center gap-2">
+            <p className="w-[70px] text-right items-center font-semibold text-primary-4 text-[16px]">Tipo:</p>
             <select
                 value={filterType}
                 onChange={(e) => setFilterType(e.target.value)}
-                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4"
+                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4 w-[150px] md:w-[150px]"
             >
                 <option>Todos</option>
                 <option>IN</option>
                 <option>OUT</option>
             </select>
+            </div>
 
+            <div className="flex flex-row items-center gap-2">
+            <p className="w-[70px] text-right items-center font-semibold text-primary-4 text-[16px]">Status:</p>
             <select
                 value={filterStatus}
                 onChange={(e) => setFilterStatus(e.target.value)}
-                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4"
+                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4 w-[150px] md:w-[150px]"
             >
                 <option>Todos</option>
                 <option>Concluída</option>
                 <option>Pendente</option>
                 <option>Cancelada</option>
             </select>
+            </div>
 
+            <div className="flex flex-row items-center gap-2">
+            <p className="w-[70px] text-right items-center font-semibold text-primary-4 text-[16px]">Data:</p>
             <select
                 value={filterPeriod}
                 onChange={(e) => setFilterPeriod(e.target.value)}
-                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4"
+                className="p-2 rounded-md border border-gray-300 bg-white text-primary-4 w-[150px] md:w-[150px]"
             >
                 <option>Todos</option>
-                <option>Útimos 7 dias</option>
-                <option>Útimos 30 dias</option>
+                <option>Últimos 7 dias</option>
+                <option>Últimos 30 dias</option>
             </select>
+            </div>
+        </div>
+
+        <div className="flex items-center gap-2 min-w-[250px] max-w-xl bg-white border-2 border-primary-1 rounded-xl p-2 shadow-sm mb-4">
+          <IoMdSwap className="text-primary-1 text-[24px]" />
+          <select
+            value={sortAmount}
+            onChange={(e) => setSortAmount(e.target.value)}
+            className="p-2 rounded-md border border-gray-300 bg-white text-primary-4 w-[200px]"
+          >
+            <option>Todos</option>
+            <option>Maior para menor</option>
+            <option>Menor para maior</option>
+          </select>
         </div>
 
         <div className="flex flex-col gap-2">
-            {filteredTransactions.map(tx => (
+            {sortedTransactions.map(tx => (
                 <div
                     key={tx.id}
                     className="relative flex items-center min-w-[350px] min-h-[100px] justify-between p-4  bg-white rounded-lg shadow hover:shadow-md transition gap-3"
--- frontend/src/app/core/data/transacao.ts
+++ frontend/src/app/core/data/transacao.ts
@@ -10,7 +10,7 @@ export type Transaction = {
 
 export const transacao: Transaction[] = [
   { id: "T001", 
-    date: "2026-02-06", 
+    date: "06/02/2026", 
     description: "Depósito via PIX", 
     type: "IN", 
     amount: 1200, 
@@ -18,10 +18,37 @@ export const transacao: Transaction[] = [
 },
 
   { id: "T002", 
-    date: "2026-02-05", 
+    date: "05/02/2026", 
     description: "Investimento RWA Agro", 
     type: "OUT", 
     amount: 350, 
     status: "Pendente" 
+},
+
+  { 
+    id: "T003", 
+    date: "20/01/2026", 
+    description: "Pagamento boleto", 
+    type: "OUT", 
+    amount: 200, 
+    status: "Concluída" 
+},
+
+  { 
+    id: "T004", 
+    date: "15/01/2026", 
+    description: "Compra de ações RWA Tech", 
+    type: "OUT", 
+    amount: 1500, 
+    status: "Cancelada" 
+},
+
+  { 
+    id: "T005", 
+    date: "01/01/2026", 
+    description: "Transferência recebida", 
+    type: "IN", 
+    amount: 800, 
+    status: "Concluída" 
 }
 ];
